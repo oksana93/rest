dcb59fcba984347c591aab3aba56de6a93792fe6
Dmi3 <m-dimon93@yandex.ru>
2017-11-12 17:12:47 +0400

    Теперь ставятся маркеры на места указанного типа (в коде :D ) Только радиус почему то не работает, показывает во всем городе места

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,50 @@
 'use strict';
+    var map;
+    var infowindow;
 
 
-    function initializeMap() {
-        var myLatlng = new google.maps.LatLng(50.022451, 36.227070);
-        var myOptions = {
-            zoom: 10,
-            scrollwheel: false,
-            center: myLatlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            mapTypeControlOptions: {
-                position: google.maps.ControlPosition.BOTTOM_LEFT
+    function initMap() {
+
+        var pyrmont = {lat: 53.212702, lng: 50.178725};
+
+        map = new google.maps.Map(document.getElementById("map_canvas"), {
+            center: pyrmont,
+            zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        var request = {
+            location: pyrmont,
+            radius: 500,
+            query: ['restaurant']
+        };
+
+        service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, callback);
+    }
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
             }
         }
     }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+        });
+    }
+$(function(){
+
+    initMap();
+})
