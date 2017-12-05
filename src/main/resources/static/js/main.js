a6efd40a46107fc017ea0834db5c2e62e9862f13
antoshina <oksana93>
2017-12-05 23:01:47 +0400

    Геолокация - версия 3!!!

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,26 +1,23 @@
 'use strict';
 var defaultPlaces = "Места отдыха";
-var placesService;
-var googleMap;
-var marketYourPosition;
+var defaultRadius = "1000";
 var iconYourPosition = "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png";
+var infoWindowForPlaces = new google.maps.InfoWindow();
+var defaultPosition = {lat: 53.212702, lng: 50.178725};
+var zoom = 15;
+
 var request;
 var options;
-var infoWindowForPlaces = new google.maps.InfoWindow();
-var yourPosition = {lat: 53.212702, lng: 50.178725};
+var placesService;
+var googleMap;
+var yourPosition;
+var marketYourPosition;
 
 $(function () {
     initMap();
 });
 
 function initMap() {
-    googleMap = new google.maps.Map(document.getElementById("map_canvas"), {
-        center: yourPosition, //центрирование
-        zoom: 15, // масштабирование
-        navigationControlOptions: { // геолокация
-            style: google.maps.NavigationControlStyle.SMALL
-        }
-    });
 
     if (navigator.geolocation) {
         options = {
@@ -28,12 +25,23 @@ function initMap() {
             timeout: 20000,
             maximumAge: 0
         };
+
         navigator.geolocation.watchPosition(
             function (position) {
+
                 yourPosition = {
                     lat: position.coords.latitude,
                     lng: position.coords.longitude
                 };
+
+                googleMap = new google.maps.Map(document.getElementById("map_canvas"), {
+                    center: yourPosition, //центрирование
+                    zoom: zoom, // масштабирование
+                    navigationControlOptions: { // геолокация
+                        style: google.maps.NavigationControlStyle.SMALL
+                    }
+                });
+
                 marketYourPosition = new google.maps.Marker({
                     map: googleMap,
                     position: yourPosition,
@@ -45,6 +53,12 @@ function initMap() {
                         fontSize: "20px"
                     }
                 });
+                google.maps.event.addListener(marketYourPosition, 'click', function () {
+                    infoWindowForPlaces.setContent("You");
+                    infoWindowForPlaces.open(googleMap, this);
+                });
+                mapRequest(yourPosition, defaultRadius, defaultPlaces);
+                service();
             },
             function (error) {
                 handleLocationError(true, infoWindowForPlaces, googleMap.getCenter());
@@ -53,15 +67,18 @@ function initMap() {
     } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindowForPlaces, googleMap.getCenter());
+
+        googleMap = new google.maps.Map(document.getElementById("map_canvas"), {
+            center: defaultPosition, //центрирование
+            zoom: zoom, // масштабирование
+            navigationControlOptions: { // геолокация
+                style: google.maps.NavigationControlStyle.SMALL
+            }
+        });
+
+        mapRequest(defaultPosition, defaultRadius, defaultPlaces);
+        service();
     }
-
-    google.maps.event.addListener(marketYourPosition, 'click', function () {
-        infoWindowForPlaces.setContent("You");
-        infoWindowForPlaces.open(googleMap, this);
-    });
-
-    mapRequest(yourPosition, "500", "театр");
-    service();
 }
 
 function mapRequest(location, radius, query) {
